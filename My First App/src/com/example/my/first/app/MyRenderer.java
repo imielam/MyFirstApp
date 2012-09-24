d6d11242586cbbfbddc20f5cfbdef26d097b7dbc
mimiela <mimiela@AMDC1275>
2012-09-24 12:43:28 +0200

    dodanie wyswietlania tojkata, ktory dostosowuje sie do wyswietlania ekranu (projecton view i camera view)

--- My First App/src/com/example/my/first/app/MyRenderer.java
+++ My First App/src/com/example/my/first/app/MyRenderer.java
@@ -1,29 +1,167 @@
 package com.example.my.first.app;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
+
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
 
-public class MyRenderer implements GLSurfaceView.Renderer{
+public class MyRenderer implements GLSurfaceView.Renderer {
 
+	private Triangle mTriangle = null;
+//	private Square mSquare = null;
+
+	private final float[] mMVPMatrix = new float[16];
+    private final float[] mProjMatrix = new float[16];
+    private final float[] mVMatrix = new float[16];
+//    private final float[] mRotationMatrix = new float[16];
+
+	public static int loadShader(int type, String shaderCode) {
+		int shader = GLES20.glCreateShader(type);
+		GLES20.glShaderSource(shader, shaderCode);
+		GLES20.glCompileShader(shader);
+		
+		return shader;
+	}
+	
+	
 	@Override
 	public void onDrawFrame(GL10 gl) {
 		GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
 		
+		Matrix.setLookAtM(mVMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 1.0f);
+		Matrix.multiplyMM(mMVPMatrix, 0, mProjMatrix, 0 , mVMatrix, 0);
+		mTriangle.draw(mMVPMatrix);
+
 	}
 
 	@Override
 	public void onSurfaceChanged(GL10 gl, int width, int height) {
 		GLES20.glViewport(0, 0, width, height);
 		
+		float ratio = (float) width / height;
+		Matrix.frustumM(mProjMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
+		System.out.println(ratio);
+//		for (float i : mProjMatrix) {
+//			System.out.print(i + ",");
+//		}
+//		System.out.println();
+
 	}
 
 	@Override
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
 		GLES20.glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+
+		mTriangle = new Triangle();
+//		mSquare = new Square();
+
+	}
+}
+
+class Triangle {
+	
+	private final String vertexShaderCode =
+	        // This matrix member variable provides a hook to manipulate
+	        // the coordinates of the objects that use this vertex shader
+	        "uniform mat4 uMVPMatrix;" +
+
+	        "attribute vec4 vPosition;" +
+	        "void main() {" +
+	        // the matrix must be included as a modifier of gl_Position
+	        "  gl_Position = vPosition * uMVPMatrix;" +
+	        "}";
+
+	private final String fragmentShaderCode =
+	        "precision mediump float;" +
+	        "uniform vec4 vColor;" +
+	        "void main() {" +
+	        "  gl_FragColor = vColor;" +
+	        "}";
+	
+	private FloatBuffer vertexBufer;
+	private final int mProgram; 
+	private int mPositionHandle;
+	private int mColorHandle;
+	private int mMVPMatrixHandle;
+	
+
+	static final int COORDS_PER_VERTEX = 3;
+	static float triangleCoords[] = { 0.0f, 0.622008459f, 0.0f, -0.5f,
+			-0.311004243f, 0.0f, 0.5f, -0.311004243f, 0.0f };
+	private final int vertexCount = triangleCoords.length / COORDS_PER_VERTEX;
+    private final int vertexStride = COORDS_PER_VERTEX * 4; // bytes per vertex
+
+	float color[] = { 0.63671875f, 0.76953125f, 0.22265625f, 1.0f };
+
+	public Triangle() {
+		ByteBuffer bb = ByteBuffer.allocateDirect(triangleCoords.length * 4);
+		bb.order(ByteOrder.nativeOrder());
+
+		vertexBufer = bb.asFloatBuffer();
+		vertexBufer.put(triangleCoords);
+		vertexBufer.position(0);
 		
+		int vertexShader = MyRenderer.loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode);
+		int fragmentShader = MyRenderer.loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode);
+		
+		mProgram = GLES20.glCreateProgram();
+		GLES20.glAttachShader(mProgram, vertexShader);
+		GLES20.glAttachShader(mProgram, fragmentShader);
+		GLES20.glLinkProgram(mProgram);
+		
+	}
+	
+	public void draw(float[] mvpMatrix) {
+		GLES20.glUseProgram(mProgram);
+		mPositionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
+		GLES20.glEnableVertexAttribArray(mPositionHandle);
+		GLES20.glVertexAttribPointer(mPositionHandle, COORDS_PER_VERTEX, GLES20.GL_FLOAT, false, vertexStride, vertexBufer);
+		mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
+		GLES20.glUniform4fv(mColorHandle, 1, color, 0);
+		
+		mMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
+		
+		GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mvpMatrix, 0);
+		
+		
+		
+		
+		GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
+		GLES20.glDisableVertexAttribArray(mPositionHandle);
+	}
+
+}
+
+class Square {
+	private FloatBuffer vertexBuffer;
+	private ShortBuffer drawListBuffer;
+
+	static final int COORDS_PER_VERTEX = 3;
+	static float squareCoords[] = { -0.5f, 0.5f, 0.0f, -0.5f, -0.5f, 0.0f,
+			0.5f, -0.5f, 0.0f, 0.5f, 0.5f, 0.0f };
+
+	private short drawOrder[] = { 0, 1, 2, 0, 2, 3 };
+
+	public Square() {
+		ByteBuffer bb = ByteBuffer.allocateDirect(squareCoords.length * 4);
+		bb.order(ByteOrder.nativeOrder());
+		vertexBuffer = bb.asFloatBuffer();
+		vertexBuffer.put(squareCoords);
+		vertexBuffer.position(0);
+
+		ByteBuffer dlb = ByteBuffer.allocateDirect(drawOrder.length * 2);
+		dlb.order(ByteOrder.nativeOrder());
+
+		drawListBuffer = dlb.asShortBuffer();
+		drawListBuffer.put(drawOrder);
+		drawListBuffer.position(0);
 	}
 
 }
